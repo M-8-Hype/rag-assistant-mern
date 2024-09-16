093b56b811622d03efd03ce361e6666851e3949c
Alexander Suesskind <s87424@bht-berlin.de>
2024-09-16 15:27:10 +0200

    CL | Add state for text input

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,15 +1,37 @@
 import styles from './Chat.module.scss'
+import { useState } from 'react'
 
 const Chat = () => {
+    const [inputText, setInputText] = useState('')
+
+    const handleChange = (e) => {
+        const { value } = e.target
+        setInputText(() => value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <>
             <div className={styles.chatOutput}>
                 <p>Chat will be displayed here</p>
             </div>
-            <div className={styles.chatInput}>
+            <form className={styles.chatInput} onSubmit={handleSubmit}>
                 <label htmlFor="name">Enter your text:</label>
-                <input type="text" name="name" id="name" required />
-            </div>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={inputText}
+                    onChange={handleChange}
+                    required
+                />
+                <button>
+                    Send
+                </button>
+            </form>
         </>
     )
 }
