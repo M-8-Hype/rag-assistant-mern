d1f29250e5490dc1e6e294e21f7bf76b9a829d67
Alexander Suesskind <s87424@bht-berlin.de>
2024-09-19 15:34:48 +0200

    CL | (TEMP) Add loading spinner

--- client/src/components/LoadingAnswer/LoadingAnswer.jsx
+++ client/src/components/LoadingAnswer/LoadingAnswer.jsx
@@ -1,12 +1,19 @@
 import styles from '../ChatOutput/ChatOutput.module.scss'
+import { ScaleLoader } from 'react-spinners'
 
 const LoadingAnswer = ({ inputText }) => {
     return (
-        <div className={styles.question}>
-            <h3>Question</h3>
-            <p>{inputText}</p>
-        </div>
-    )
+        <>
+            <div className={styles.question}>
+                <h3>Question</h3>
+                <p>{inputText}</p>
+            </div>
+            <div className={styles.answer}>
+                <h3>Answer</h3>
+                <ScaleLoader color={"#FFFFFF"} height={30} width={10} margin={5} radius={5} />
+            </div>
+        </>
+)
 }
 
 export default LoadingAnswer
