909a6859de805a4c246164ab720c072705b1ee1a
Alexander Suesskind <s87424@bht-berlin.de>
2024-09-29 18:17:01 +0200

    CL | Add texts for instruction page

--- client/src/pages/InstructionPage/InstructionPage.jsx
+++ client/src/pages/InstructionPage/InstructionPage.jsx
@@ -1,13 +1,39 @@
 import { Link } from 'react-router-dom'
+import { INSTRUCTION } from '../../utils/texts.js'
 
 const InstructionPage = () => {
+    const instructionSteps = INSTRUCTION.steps.map((step, index) => {
+        return (
+            <li key={index}>
+                <p>{step.text}</p>
+                {step.subItems && <ol>
+                    {step.subItems && step.subItems.map((subItem, subIndex) => {
+                        return (
+                            <li key={subIndex}>
+                                <p>{subItem.text}</p>
+                            </li>
+                        )
+                    })}
+                </ol>}
+                {step.conclusion && <p>{step.conclusion}</p>}
+            </li>
+        )
+    })
+
     return (
         <div>
             <Link to="/settings">
                 <button>Next Page</button>
             </Link>
             <h2>Instructions</h2>
-            <p>Instructions go here...</p>
+            <div>
+                <p>{INSTRUCTION.introduction}</p>
+                <ul>{instructionSteps}</ul>
+                <p>{INSTRUCTION.conclusion}</p>
+            </div>
+            <Link to="/settings">
+                <button>View Settings</button>
+            </Link>
         </div>
     )
 }
---
+++ client/src/utils/texts.js
@@ -0,0 +1,33 @@
+export const INSTRUCTION = {
+    introduction: "Please read the following instructions carefully:",
+    steps: [
+        {
+            text: "On the next page (Settings), first select your username. If it is not in the dropdown list, click on the button to create a new user for yourself. If you have chosen a username, do not change it and keep it until the end of the trial."
+        },
+        {
+            text: "Then select your desired language for the output of the answers. Please note: You can write your question to the system in both English and German, regardless of the language selected."
+        },
+        {
+            text: "At the end of the settings selection, select the game for which you would like questions answered. Then press the corresponding button to go to the next page."
+        },
+        {
+            text: "You are now at the core of the application (Chat). Here you can ask questions about the game you have selected. You can ask questions in both English and German. The system will answer your questions in the language you have selected in the settings. For this trial, the procedure is largely predetermined. Therefore, please follow the instructions and do not deviate from them, as much as possible.",
+            subItems: [
+                {
+                    text: "TBD 1"
+                },
+                {
+                    text: "TBD 2"
+                }
+            ],
+            conclusion: "Once you have completed each step, click on the corresponding button to go to the next page."
+        },
+        {
+            text: "On this page (Report) you now have the option of saving the chat you have had with the system as a text file. Please be sure to complete this step, otherwise the results and evaluation will not be possible. As soon as you press this button, the chat history will be saved in the folder structure of the application, but you also have the option of saving it in a location of your choice. To be on the safe side, please check the contents of the saved text file."
+        },
+        {
+            text: "You can now close this application according to the readme file. Thank you for your cooperation!"
+        }
+    ],
+    conclusion: "If you have fully understood the instructions, please click on the button below."
+}
